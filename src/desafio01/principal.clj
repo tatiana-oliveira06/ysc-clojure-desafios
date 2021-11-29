7054a17e1f6334a68c16180d871f75bd1c3994e1
Tatiana Oliveira <tatianavln2012@gmail.com>
2021-11-29 13:09:13 -0300

    Primeiro commit

---
+++ src/desafio01/principal.clj
@@ -0,0 +1,83 @@
+(ns desafio01.principal
+  (:use clojure.pprint)
+  (:require [java-time :as time]))
+
+(def cliente01 {:id-cliente 1 :nome "Maria da Silva" :cpf "12356743565" :email "maria@gmal.com"
+                :cartao     {:numero  "1324567897643256" :cvv "123" :validade "03/2030" :limite 2000.00
+                             :compras [{:data "01/11/2021" :valor 200.00 :estabelecimento "Extra super SA" :categoria "Alimentação"}
+                                       {:data "23/09/2020" :valor 545.00 :estabelecimento "Extra super SA" :categoria "Saúde"}
+                                       {:data "03/04/2021" :valor 78.67 :estabelecimento "Extra super SA" :categoria "Alimentação"}
+                                       {:data "15/01/2020" :valor 21.30 :estabelecimento "Extra super SA" :categoria "Saúde"}
+                                       {:data "12/11/2021" :valor 567.99 :estabelecimento "Extra super SA" :categoria "Educação"}]}})
+
+;OBTER COMPRAS
+
+(defn obter-compras [cliente]
+  (-> cliente
+      :cartao
+      :compras))
+
+
+; AGRUPAMENTO POR CATEGORIA
+(defn calcula-total-da-categoria [[categoria compras]]
+  (let [valor-total-da-categoria (reduce + (map :valor compras))]
+    [categoria valor-total-da-categoria]))
+
+(defn calcula-gastos-totais-por-categoria
+  [compras]
+  (map calcula-total-da-categoria compras))
+
+(defn agrupa-por-categoria [cliente]
+  (->> (obter-compras cliente)
+       (group-by :categoria)
+       (calcula-gastos-totais-por-categoria)
+       (pprint)))
+
+;agrupa-por-categoria cliente01)
+
+;FATURA DO MES
+(defn mes-desejado? [compra periodo]
+  (let [mes-corrente periodo
+        mes-da-fatura (time/format "MM/yyyy" (time/local-date "dd/MM/yyyy" (:data compra)))]
+    (= mes-corrente mes-da-fatura))
+  )
+
+(defn calcula-total-do-mes [compras periodo]
+  (reduce + (map :valor (filter #(mes-desejado? % periodo) compras))))
+
+(defn calcula-valor-da-fatura [cliente periodo]
+  (-> (obter-compras cliente)
+      (calcula-total-do-mes periodo)
+      (pprint)))
+
+;(calcula-valor-da-fatura cliente01 "04/2021")
+
+;BUSCA DE COMPRAS PELO ESTABELECIMENTO OU VALOR
+
+(defn corresponde? [compra filtro]
+  (let [estabelecimento (:estabelecimento compra)
+        valor (:valor compra)]
+    (if (string? filtro)
+      (= estabelecimento filtro)
+      (> valor filtro))))
+
+(defn aplica-filtro [compras filtro]
+  (filter #(corresponde? % filtro) compras))
+
+(defn busca-compras [cliente filtro]
+  (-> (obter-compras cliente)
+      (aplica-filtro filtro)
+      (pprint))
+  )
+
+(busca-compras cliente01 300M)
+
+
+
+
+
+
+
+
+
+
